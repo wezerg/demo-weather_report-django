229ccdf9a40085f7cd05362e3eb38d824ec1dc57
chikaakta <59062753+chikaakta@users.noreply.github.com>
2021-06-08 14:40:51 +0200

    dd

--- mysite/timer/views.py
+++ mysite/timer/views.py
@@ -1,11 +1,28 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+from datetime import datetime
+import pytz
 
-# Create your views here.
+
+def get_date(tz):
+    x = datetime.now(tz)
+    return x.strftime('%Y-%m-%d %H:%M:%S %Z %z')
+
 
 def index(request):
-    template = loader.get_template('timer/index.html')
+
+    timeZ_Kl = pytz.timezone('Asia/Kolkata')
+    timeZ_Ny = pytz.timezone('America/New_York')
+    timeZ_Ma = pytz.timezone('Africa/Maseru')
+    timeZ_Ce = pytz.timezone('US/Central')
+    timeZ_At = pytz.timezone('Europe/Athens')
+
     context = {
-        'latest_question_list': "",
+        'latest_question_list': [
+                                    {"question_text":get_date(timeZ_Kl)},
+                                    {"question_text":get_date(timeZ_Ny)},
+                                    {"question_text":get_date(timeZ_Ma)},
+                                    {"question_text":get_date(timeZ_Ce)},
+                                    {"question_text":get_date(timeZ_At)},
+                                 ]
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'timer/index.html', context)
