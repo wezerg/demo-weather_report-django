34735f8521a5e76a2f1d154d01c359f081dd4184
Wezerg <vivian.grimaux@hotmail.fr>
2021-06-09 14:56:13 +0200

    remove polls application and add bootstrap

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'timer.apps.TimerConfig',
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/timer/static/timer/javascript.js
+++ mysite/timer/static/timer/javascript.js
@@ -14,13 +14,20 @@ setInterval(() => {
             document.getElementById(loc.localisation).children[1].children[1].innerHTML = loc.date
             document.getElementById(loc.localisation).children[2].children[1].innerHTML = loc.heure
             let hour = parseInt(loc.heure.substring(0,2));
-            if (hour > 6 && hour < 19) {
+            if (hour > 4 && hour < 12) {
+                document.getElementById(loc.localisation).children[3].children[0].setAttribute("src", "../../static/timer/sunrise.png");
+                document.getElementById(loc.localisation).children[3].children[0].classList.remove(['imgNight', 'imgDay']);
+                document.getElementById(loc.localisation).children[3].children[0].classList.add('imgMorning');
+            }
+            else if(hour > 12 && hour < 20){
                 document.getElementById(loc.localisation).children[3].children[0].setAttribute("src", "../../static/timer/sun.png");
-                document.getElementById(loc.localisation).children[3].children[0].classList.replace('imgNight', 'imgDay');
+                document.getElementById(loc.localisation).children[3].children[0].classList.remove(['imgMorning', 'imgNight']);
+                document.getElementById(loc.localisation).children[3].children[0].classList.add('imgDay');
             }
             else{
                 document.getElementById(loc.localisation).children[3].children[0].setAttribute("src", "../../static/timer/night.png");
-                document.getElementById(loc.localisation).children[3].children[0].classList.replace('imgDay', 'imgNight');
+                document.getElementById(loc.localisation).children[3].children[0].classList.remove(['imgMorning', 'imgDay']);
+                document.getElementById(loc.localisation).children[3].children[0].classList.add('imgNight');
             }
         }
     })
